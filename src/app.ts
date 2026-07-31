339ac51132217ab6425dd017b671b395d3518cc0
unknown <sadmansakib8530@gmail.com>
2026-08-01 01:52:16 +0600

    fix: increase rate limit in development mode

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 200,
+    max: process.env.NODE_ENV === 'production' ? 200 : 2000,
     standardHeaders: true,
     legacyHeaders: false,
   })
